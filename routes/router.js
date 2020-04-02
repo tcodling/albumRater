3b80c35996e87e0468c9fb0473facd94855dd53d
simon-hall-970 <60405692+simon-hall-970@users.noreply.github.com>
2020-04-03 11:43:07 +1300

    Revert "create individual album route with dummy data object for test"

--- routes/router.js
+++ routes/router.js
@@ -4,20 +4,4 @@ const db = require('../db')
 
 const router = express.Router()
 
-
-router.get('/album/:id', (req, res) =>{
-    dummyData ={
-        name: "Album Title",
-        cover: "Image goes here",
-        artist: "The Bestest Group Ever",
-        year: "2019",
-        comments: [
-            {rating: "9", comment:"This is the first comment"},
-            {rating: "9", comment:"This is the first comment"},
-            {rating: "9", comment:"This is the first comment"}
-        ]
-    }
-    res.render('new.hbs', dummyData)
-})
-
 module.exports = router
