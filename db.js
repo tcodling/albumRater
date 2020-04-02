2f825729c41b0fdd653b14106f2ad6e99fc9b4d4
tcodling <thomas.codling@outlook.com>
2020-04-03 11:17:42 +1300

    Add homepage partial

--- db.js
+++ db.js
@@ -2,6 +2,11 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
+//functions
+
+
+
+//export functions
 module.exports = {
   
 }
